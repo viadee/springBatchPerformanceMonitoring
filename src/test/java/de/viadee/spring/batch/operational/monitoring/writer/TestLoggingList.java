95d65c7f5733e24b9c42b8e03752a864dff9d83a
Frank Köhne <frank.koehne@viadee.de>
2018-07-16 14:06:29 +0200

    GmbH -> AG.

--- src/test/java/de/viadee/spring/batch/operational/monitoring/writer/TestLoggingList.java
+++ src/test/java/de/viadee/spring/batch/operational/monitoring/writer/TestLoggingList.java
@@ -1,5 +1,5 @@
 /**
- * Copyright � 2016, viadee Unternehmensberatung GmbH
+ * Copyright � 2016, viadee Unternehmensberatung AG
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
